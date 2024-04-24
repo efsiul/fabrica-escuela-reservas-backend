b20d9bf8d072031eb9de4c8f4e4458acb607f748
Luis Felipe Cadavid <felipe.cadavid@udea.edu.co>
2024-04-24 01:10:39 -0500

    feat: add authorize cors in project

---
+++ src/main/java/com/fabricaescuela20241/reserva/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.fabricaescuela20241.reserva;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import lombok.NonNull; // Import the NonNull type
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
